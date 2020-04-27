56e46a36b21355abd09d2260790f378d62eea5e4
bokimilinkovic <boki1997@gmail.com>
2020-04-27 14:37:15 +0200

    Admin front page part 1

--- client/src/components/AdminPage.vue
+++ client/src/components/AdminPage.vue
@@ -16,7 +16,7 @@
           <b-dropdown-item v-on:click="registerfirm=true">Firm</b-dropdown-item>
         </b-dropdown>
         
-        <b-button style="margin:5px;">Comments</b-button>
+        <b-button style="margin:5px;" v-on:click="showcomments = true">Comments</b-button>
       </b-button-group>
       <br>
        <br>
@@ -45,16 +45,24 @@
         </table>
 
 
-         <table class="table table-bordered" id="car-brand" v-if="showbrands" @mouseleave="showbrands=false">
+         <table class="table table-bordered" id="car-brand" v-if="showbrands && this.brands.length>0" @mouseleave="showbrands=false">
           <thead>
             <tr>
               <th scope="col">Name of the brand</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="brand in this.brands" :key="brand.id">
-              <th scope="row">{{brand.name}}</th>
-              <td><button type="button" class="btn btn-danger" v-on:click="removeBrand(brand.name)">Remove brand</button></td>
+            <tr v-for="brand in this.brands" :key="brand.id" >
+              <th scope="row">{{brand}}</th>
+              <td><button type="button" class="btn btn-danger" v-on:click="removeBrand(brand)">Remove brand</button></td>
+            </tr>
+            <tr>
+              <td><button type="button" class="btn btn-primary" @click="addNewBrand=true">Add new brand</button></td>
+              <td v-if="addNewBrand">
+                <form> 
+                  <b-input type="text" v-model="newBrand"  class="form-control"/><button type="button" class="btn btn-success" @click="AddNewBrand()">Submit</button>
+                </form>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -67,8 +75,16 @@
           </thead>
           <tbody>
             <tr v-for="model in this.models" :key="model.id">
-              <th scope="row">{{model.name}}</th>
-              <td><button type="button" class="btn btn-danger" v-on:click="removeModel(model.name)">Remove model</button></td>
+              <th scope="row">{{model}}</th>
+              <td><button type="button" class="btn btn-danger" v-on:click="removeModel(model)">Remove model</button></td>
+            </tr>
+            <tr>
+              <td><button type="button" class="btn btn-primary" @click="addNewModel=true">Add new model</button></td>
+              <td v-if="addNewModel">
+                <form> 
+                  <b-input type="text" v-model="newModel"  class="form-control"/><button type="button" class="btn btn-success" @click="AddNewModel()">Submit</button>
+                </form>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -82,8 +98,16 @@
           </thead>
           <tbody>
             <tr v-for="fuel in this.fuels" :key="fuel.id">
-              <th scope="row">{{fuel.name}}</th>
-              <td><button type="button" class="btn btn-danger" v-on:click="removeFuel(fuel.name)">Remove fuel type</button></td>
+              <th scope="row">{{fuel}}</th>
+              <td><button type="button" class="btn btn-danger" v-on:click="removeFuel(fuel)">Remove fuel type</button></td>
+            </tr>
+            <tr>
+              <td><button type="button" class="btn btn-primary" @click="addNewFuel=true">Add new fuel</button></td>
+              <td v-if="addNewFuel">
+                <form> 
+                  <b-input type="text" v-model="newFuel"  class="form-control"/><button type="button" class="btn btn-success" @click="AddNewFuel()">Submit</button>
+                </form>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -96,8 +120,16 @@
           </thead>
           <tbody>
             <tr v-for="tr in this.transmissions" :key="tr.id">
-              <th scope="row">{{tr.name}}</th>
-              <td><button type="button" class="btn btn-danger" v-on:click="removeTransmission(tr.name)">Remove transmission type</button></td>
+              <th scope="row">{{tr}}</th>
+              <td><button type="button" class="btn btn-danger" v-on:click="removeTransmission(tr)">Remove transmission type</button></td>
+            </tr>
+            <tr>
+              <td><button type="button" class="btn btn-primary" @click="addNewTrans=true">Add new transmission type</button></td>
+              <td v-if="addNewTrans">
+                <form> 
+                  <b-input type="text" v-model="newTransms"  class="form-control"/><button type="button" class="btn btn-success" @click="AddNewTransms()">Submit</button>
+                </form>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -109,9 +141,17 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="cl in this.classes" :key="cl.id">
-              <th scope="row">{{cl.name}}</th>
-              <td><button type="button" class="btn btn-danger" v-on:click="removeClass(cl.name)">Remove car type</button></td>
+            <tr v-for="cl in this.carClasses" :key="cl.id">
+              <th scope="row">{{cl}}</th>
+              <td><button type="button" class="btn btn-danger" v-on:click="removeClass(cl)">Remove car type</button></td>
+            </tr>
+            <tr>
+              <td><button type="button" class="btn btn-primary" @click="addNewClass=true">Add new car class</button></td>
+              <td v-if="addNewClass">
+                <form> 
+                  <b-input type="text" v-model="newClass"  class="form-control"/><button type="button" class="btn btn-success" @click="AddNewClass()">Submit</button>
+                </form>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -152,7 +192,21 @@
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-success" @click="register()" data-dismiss="modal" > <b-icon icon="check-circle" aria-hidden="true"> </b-icon> Confirm</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal"> <b-icon icon="x-circle"> </b-icon>  Close</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal" @click="resetValues()"> <b-icon icon="x-circle"> </b-icon>  Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br>
+        <div class="row">
+          <div v-for="comment in this.comments" :key="comment.id"  class="column">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="../assets/comment.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">{{comment.recezent}} for {{comment.Oglas}}</h5>
+                <p class="card-text">{{comment.Komentar}}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -163,7 +217,7 @@
 </template>
 
 <script>
-//import axios from 'axios'
+import axios from 'axios'
 export default {
     name: 'AdminPage',
     data(){
@@ -172,21 +226,11 @@ export default {
           { type: 'user', blocked: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
           { type:'firm', blocked: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } }
           ],
-          brands: [
-            { name: 'Opel'}, {name: 'Wolksvagen'},{name: 'Fiat'}
+          comments:[
+            {recezent: 'Marko', Oglas: 'Auto', Komentar: 'Auto je krsina'},
+            {recezent: 'Jovan', Oglas: 'Traktor', Komentar: 'Ide, trazi cetvrtu'}
           ],
-          models: [
-            {name:'Astra'},{name:'Passat'},{name:'Stilo'}
-          ],
-          fuels: [
-            {name:'Gas'},{name:'Diesel'},{name:'Gasoline'}
-          ],
-          transmissions: [
-            {name:'manual'},{name:'automatic'}
-          ],
-          classes: [
-            {name:'old-timer'},{name:'sport'},{name:'economic'}
-          ],
+         
           firstLastName: '',
           firmName: '',
           email: '',
@@ -198,8 +242,12 @@ export default {
           showtransmissions: false,
           showfuels: false,
           showclasses: false,
-          registeruser: false,
+          registeruser: false, showcomments: false,
           registerfirm: false,
+          addNewBrand: false, addNewModel:false, addNewFuel: false, addNewTrans: false, addNewClass: false,
+          newBrand: '', newModel:'', newFuel: '', newTransms: '',newClass: '',
+          brands:[], carClasses:[],transmissions:[],fuels:[],models:[],modelBool:true,brandsResponse:[]
+
        }
     },
 
@@ -234,10 +282,115 @@ export default {
              this.registerfirm = !this.registerfirm
         }
         
+      },
+      resetValues(){
+        this.registeruser = false 
+        this.registerfirm = false
+      },
+
+      getCarSpec(){
+        axios.get('/api/brands').then(response => { 
+         // stavi brandove u brands, a mozda samo stringove ubaci     
+        this.brandsResponse = response.data;
+        this.brands = []
+
+        for(let b of this.brandsResponse){
+          this.brands.push(b.Name);
+          for(let m of b.Models){
+            this.models.push(m)
+          }
+        }
+      });
+      
+      axios.get('/api/classes').then(response => { 
+        this.carClasses = []
+        for(let cl of response.data)
+          this.carClasses.push(cl)
+      });
+
+      axios.get('/api/fuels').then(response => { 
+        this.fuels = []
+        for(let fl of response.data)
+          this.fuels.push(fl);
+      });
+
+      axios.get('/api/transmissions').then(response => { 
+        this.transmissions = []
+        for(let tr of response.data)
+          this.transmissions.push(tr);
+      });
+      },
+
+      AddNewModel(){
+        console.log(this.newModel)
+        if (this.newModel != ''){
+            this.models.push(this.newModel)
+            this.addNewModel = false
+        }
+        this.addNewModel = false
+      },
+        AddNewBrand(){
+        console.log(this.newBrand)
+        if (this.newBrand != ''){
+            this.brands.push(this.newBrand)
+            this.addNewBrand = false
+        }
+        this.addNewBrand = false
+      },
+       AddNewFuel(){
+        console.log(this.newFuel)
+        if (this.newFuel != ''){
+            this.fuels.push(this.newFuel)
+            this.addNewFuel = false
+        }
+        this.addNewFuel = false
+      },
+      AddNewTransms(){
+        console.log(this.newTransms)
+        if (this.newTransms != ''){
+            this.transmissions.push(this.newTransms)
+            this.addNewTrans = false
+        }
+        this.addNewTrans = false
+      },
+      AddNewClass(){
+        console.log(this.newClass)
+        if (this.newClass != ''){
+            this.carClasses.push(this.newClass)
+            this.addNewClass = false
+        }
+        this.addNewClass = false
       }
+
+
+
+
     },
     created(){
-
+      this.getCarSpec();
     }
 }
 </script>
+<style scoped>
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+</style>
