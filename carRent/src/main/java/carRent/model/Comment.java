36c449e6763b1b1e38b0aa287cd68863a4a8fd1d
Stefan Jokic <stefanpelejokic@gmail.com>
2020-05-06 14:29:13 +0200

    add class comment

---
+++ carRent/src/main/java/carRent/model/Comment.java
@@ -0,0 +1,77 @@
+package carRent.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = false, nullable = false)
+    private LocalDateTime created;
+
+    @Column(unique = false, nullable = false)
+    private String body;
+
+    @ManyToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="car_id",referencedColumnName = "id")
+    private Car car;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
+
+    public Comment() {
+    }
+
+    public Comment(LocalDateTime created, String body, Car car, User user) {
+        this.created = created;
+        this.body = body;
+        this.car = car;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
