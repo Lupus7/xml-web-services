0d9d9177a9957a1f943e2a1be34d9962d0a45465
Lupus7 <sinisa.canak997@gmail.com>
2020-05-21 16:01:22 +0200

    initial xml schema

---
+++ xml/cars/Ad.java
@@ -0,0 +1,170 @@
+
+package cars;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for Ad complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Ad"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}long"/&gt;
+ *         &lt;element name="startDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *         &lt;element name="endDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *         &lt;element name="place" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="car" type="{xml-web-services}Car"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Ad", namespace = "xml-web-services", propOrder = {
+    "id",
+    "startDate",
+    "endDate",
+    "place",
+    "car"
+})
+public class Ad {
+
+    @XmlElement(namespace = "xml-web-services")
+    protected long id;
+    @XmlElement(namespace = "xml-web-services", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar startDate;
+    @XmlElement(namespace = "xml-web-services", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar endDate;
+    @XmlElement(namespace = "xml-web-services", required = true)
+    protected String place;
+    @XmlElement(namespace = "xml-web-services", required = true)
+    protected Car car;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     */
+    public void setId(long value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the startDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Sets the value of the startDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setStartDate(XMLGregorianCalendar value) {
+        this.startDate = value;
+    }
+
+    /**
+     * Gets the value of the endDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * Sets the value of the endDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setEndDate(XMLGregorianCalendar value) {
+        this.endDate = value;
+    }
+
+    /**
+     * Gets the value of the place property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPlace() {
+        return place;
+    }
+
+    /**
+     * Sets the value of the place property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPlace(String value) {
+        this.place = value;
+    }
+
+    /**
+     * Gets the value of the car property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Car }
+     *     
+     */
+    public Car getCar() {
+        return car;
+    }
+
+    /**
+     * Sets the value of the car property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Car }
+     *     
+     */
+    public void setCar(Car value) {
+        this.car = value;
+    }
+
+}
---
+++ xml/cars/ObjectFactory.java
@@ -0,0 +1,96 @@
+
+package cars;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the cars package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: cars
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Car }
+     * 
+     */
+    public Car createCar() {
+        return new Car();
+    }
+
+    /**
+     * Create an instance of {@link Ad }
+     * 
+     */
+    public Ad createAd() {
+        return new Ad();
+    }
+
+    /**
+     * Create an instance of {@link Booking }
+     * 
+     */
+    public Booking createBooking() {
+        return new Booking();
+    }
+
+    /**
+     * Create an instance of {@link Brand }
+     * 
+     */
+    public Brand createBrand() {
+        return new Brand();
+    }
+
+    /**
+     * Create an instance of {@link Model }
+     * 
+     */
+    public Model createModel() {
+        return new Model();
+    }
+
+    /**
+     * Create an instance of {@link Class }
+     * 
+     */
+    public Class createClass() {
+        return new Class();
+    }
+
+    /**
+     * Create an instance of {@link Fuel }
+     * 
+     */
+    public Fuel createFuel() {
+        return new Fuel();
+    }
+
+    /**
+     * Create an instance of {@link Transmission }
+     * 
+     */
+    public Transmission createTransmission() {
+        return new Transmission();
+    }
+
+}
