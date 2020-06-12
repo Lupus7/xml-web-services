c5a751413fccecd3072fa9003908ceeb25bd977e
Lupus7 <sinisa.canak997@gmail.com>
2020-06-12 19:14:16 +0200

    delete admin app

--- admin-app/src/main/java/team10/admin/repositories/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package team10.admin.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import team10.admin.models.Cart;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
--- admin-app/src/main/java/team10/admin/repositories/FuelRepository.java
+++
@@ -1,9 +0,0 @@
-package team10.admin.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import team10.admin.models.Fuel;
-
-@Repository
-public interface FuelRepository extends JpaRepository<Fuel, Long> {
-}
