be97ada384f5036c81ab810acc7e575d71144a83
Lupus7 <sinisa.canak997@gmail.com>
2020-06-21 23:53:59 +0200

    fix

--- client/src/components/BookingSubmitForm.vue
+++ client/src/components/BookingSubmitForm.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
         <!-- NEW BOOKING FORM -->
-        <div class="modal fade" id="newbooking" tabindex="-1" role="dialog" aria-hidden="true">
+        <div
+            class="modal fade"
+            id="newbooking"
+            tabindex="-1"
+            role="dialog"
+            aria-hidden="true"
+            ref="my-modal"
+        >
             <div class="modal-dialog">
                 role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">New Booking</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
@@ -15,35 +27,74 @@
                         <b-form>
                             <div>
                                 <div class="form-group">
+                                    <label>Ad</label>
+                                    <b-form-select
+                                        v-model="selected"
+                                        :options="options"
+                                    ></b-form-select>
+                                </div>
+
+                                <div class="form-group">
                                     <label>Place</label>
-                                    <b-input type="text" class="form-control" />
+                                    <b-input
+                                        type="text"
+                                        class="form-control"
+                                        v-model="place"
+                                    />
                                 </div>
 
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label>Start Date</label>
                                         <b-form-datepicker
-                                            :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }"
+                                            :date-format-options="{
+                                                year: 'numeric',
+                                                month: '2-digit',
+                                                day: '2-digit',
+                                            }"
+                                            v-model="startDate"
+                                            :min="new Date()"
                                             locale="en"
+                                            :disabled="selected == -1"
                                         ></b-form-datepicker>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label>End Date</label>
                                         <b-form-datepicker
-                                            :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }"
+                                            :date-format-options="{
+                                                year: 'numeric',
+                                                month: '2-digit',
+                                                day: '2-digit',
+                                            }"
+                                            v-model="endDate"
+                                            :min="new Date()"
                                             locale="en"
+                                            :disabled="selected == -1"
                                         ></b-form-datepicker>
                                     </div>
                                 </div>
-
-                               
                             </div>
                         </b-form>
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-success" data-dismiss="modal">
-                            <b-icon icon="check-circle" aria-hidden="true"></b-icon>Create Booking
+                        <button
+                            type="button"
+                            class="btn btn-success"
+                            data-dismiss="modal"
+                            @click="createBooking()"
+                            :disabled="
+                                selected == -1 ||
+                                    place == '' ||
+                                    startDate == '' ||
+                                    endDate == ''
+                            "
+                        >
+                            <b-icon
+                                icon="check-circle"
+                                aria-hidden="true"
+                            ></b-icon
+                            >Create Booking
                         </button>
                         <button
                             type="button"
@@ -60,23 +111,47 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
     data() {
         return {
-        
-
+            selected: -1,
+            options: [{ value: -1, text: "Please select an ad..." }],
+            startDate: "",
+            endDate: "",
+            place: "",
         };
     },
     methods: {
-    
+        createBooking() {
+            axios.post("/rent/api/booking", {
 
-       
+                adId: this.selected,
+                startDate: this.startDate + "T00:00:00",
+                endDate: this.endDate + "T00:00:00",
+                place: this.place,
+            });
+        },
     },
     created() {
-    }
+        axios.get("/cars-ads/api/ad/client").then((response) => {
+            response.data.forEach((element) => {
+                this.options.push({
+                    value: element.adId,
+                    text:
+                        "#" +
+                        element.adId +
+                        ": " +
+                        element.brand +
+                        " " +
+                        element.model +
+                        " ",
+                });
+            });
+        });
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
